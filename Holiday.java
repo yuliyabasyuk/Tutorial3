3c4210f0224ce0dab0ce4ec0d064fbec4f614178
yuliyabasyuk <yuliya.basyuk.92@gmail.com>
2018-04-23 18:51:15 +0300

    added Holiday.java

---
+++ Holiday.java
@@ -0,0 +1,40 @@
+public class Holiday {
+	  private String name;
+	  private int day;
+	  private String month;
+	
+	public Holiday (String name, int day, String month) {
+		this.name = name;
+		this.day = day;
+		this.month = month;
+	}
+	
+	public void setDay(int day) {
+		if((day > 0) & (day < 32)) {
+			this.day = day;
+		} 	else {
+			System.out.println("Enter the valid day ");
+			}
+	}
+	
+	/*a method inSameMonth compares two instances of the class Holiday,and
+	returns the Boolean value true if they have the same month, and false if they do not.*/
+	
+	public static boolean inSameMonth(Holiday h1, Holiday h2) {
+		if (h1.month.equals(h2.month)) {
+			return true;
+		} 
+		return false;
+	}
+	
+	public int getDay() {
+		return day;
+	}	
+	
+}
+		
+
+
+
+
+
